c55ed332872ea9cd4c44468ef1838a3163337f2c
georges-circuits <41122948+georges-circuits@users.noreply.github.com>
2020-01-13 12:11:05 +0100

    added descriptions

--- site.php
+++ site.php
@@ -1,8 +1,9 @@
 <!-- Hello, stranger :) -->
+
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
         <title>My site</title>
         <style type="text/css">
             <?php
@@ -11,7 +12,7 @@
                 }
                 
                 // visual page setup
-                $margin_left_right = "10%";
+                $margin_left_right = "5%";
                 if($_GET["theme"] === "light") 
                 {
                     $background = "#FFFFFF";
@@ -35,10 +36,17 @@
                 <?php echo "margin-right: $margin_left_right;\n"; ?>
                 <?php echo "margin-left: $margin_left_right;\n"; ?>
             }
-            p, ul {
+            p {
                 <?php echo "color: $foreground_text;\n"; ?>
                 font-size: 16px;
-                padding: 5px;
+            }
+            ul {
+                <?php echo "color: $foreground_text;\n"; ?>
+                font-size: 16px;
+            }
+            ul li {
+                padding: 3px;
+                margin-left: -10px;
             }
             a:link, a:visited {
                 <?php echo "color: $foreground_a;\n"; ?>
@@ -62,6 +70,7 @@
                 margin: 0 auto;
                 max-height: 100%;
                 max-width: 100%;
+                margin-left: 10px;
             }
             * {
                 box-sizing: border-box;
@@ -69,7 +78,6 @@
             .box {
                 float: left;
                 width: 50%;
-                padding: 5px;
             }
             .clearfix::after {
                 content: "";
@@ -88,27 +96,29 @@
 
         <p>This site is meant to be kind of a guidepost of my internet footprint. Maybe even a personal blog of sorts. It is also one of those sideprojects that, unlike many, actually made it into something tangible. As such however, I cannot promise its long-term permanence (the possibility of me forgetting about the relentless domain expiration date is very plausible for example)...</p>
 
-        <p>Most of my older projects (mid 2019 and older) can be found on my, now no longer active, <a href="https://www.instagram.com/georges_circuits/" target="_blank">Instagram</a> account. I divesified and became less active on social media since (still don't know whether there is a correlation, anyways). I am on <a href="https://twitter.com/jiri_manak" target="_blank">Twitter</a>, but I am certainly not very active by Twitter's standards. I also sometimes post on <a href="https://www.youtube.com/channel/UCNN9n9_ZPvUgNaIL2yDP8Qw" target="_blank">YouTube</a> (and subsequently <a href="https://lbry.tv/@George:f" target="_blank">LBRY</a>) but those are even worse off activity-wise. I also have <a href="https://github.com/georges-circuits/" target="_blank">GitHub</a> with some public projects and a few more private ones, I might release them if I ever get around to tidy them up.</p>
+        <p>Most of my older projects (mid 2019 and older) can be found on my, now no longer active, <a href="https://www.instagram.com/georges_circuits/" target="_blank">Instagram</a> account. I diversified and became less active on social media since (still don't know whether there is a correlation, anyways). I am on <a href="https://twitter.com/jiri_manak" target="_blank">Twitter</a>, but I am certainly not very active by Twitter's standards. I also sometimes post on <a href="https://www.youtube.com/channel/UCNN9n9_ZPvUgNaIL2yDP8Qw" target="_blank">YouTube</a> (and subsequently <a href="https://lbry.tv/@George:f" target="_blank">LBRY</a>) but those are even worse off activity-wise. I also have <a href="https://github.com/georges-circuits/" target="_blank">GitHub</a> with some public projects and a few more private ones, I might release them if I ever get around to tidy them up.</p>
 
         
         <h1 style="margin-top: 50px;">2020</h1>
+
+        <h2><i>In progress:</i> Facebook datamining</h2>
         <div class="clearfix">
             <div class="box">
-                <h2><i>In progress:</i> Facebook datamining</h2>
-                <p>How a random weekend project turned into my graduation work... At the beggining it was just that. I wanted to learn how to work with databases. I figured that instead of using some random JSON example I could download my data from Facebook and use that instead.</p>
-                <p>I'm already satisfied with the basic functionality (the original objective) - the Python script counts the amount of messages in a specified timeframe, say a week, resulting in a messages per week value which it then puts into a .csv chart. I can then take this file, open it in Excel and make a nice graph out of it.</p>
-                <p>I recently also implemented the most used words counter. I'm planing to add a couple more features and make the script more interactive and intuitive to use. I haven't yet figured out whether this data could be used for at least rudimentary psychological research.</p>
+                <p>How a random weekend project turned into my graduation work... At the beginning it was just that. I wanted to learn how to work with databases. I figured that instead of using some random JSON example I could download my data from Facebook and use that instead.</p>
+                <p>I'm already satisfied with the basic functionality (the original objective) - the Python script counts the amount of messages in a specified time frame, say a week, resulting in a messages per week value which it then puts into a .csv chart. I can then take this file, open it in Excel and make a nice graph out of it.</p>
+                <p>I recently also implemented the most used words counter. I'm planning to add a couple more features and make the script more interactive and intuitive to use. I haven't yet figured out whether this data could be used for at least rudimentary psychological research</p>
             </div>
             <div class="box">
                 <?php fp_img("facebook_graph.gif"); ?>
             </div>
         </div>
+
+        <h2><i>In progress:</i> Wireless soil moisture sensors</h2>
         <div class="clearfix">
             <div class="box">
-                <h2><i>In progress:</i> Wireless soil moisture sensors</h2>
-                <p>A network of ultra-low power low-cost maintnance-free outdoor wireless sensors connected to a base station to provide soil moisture readout for monitoring or automatic watering purposes.</p>
+                <p>A network of ultra-low power low-cost maintenance-free outdoor wireless sensors connected to a base station to provide soil moisture readout for monitoring or automatic watering purposes.</p>
                 <p>In the works since the summer of 2019.</p>
-                <p><a href="/images/base_drawing.gif">base_drawing</a></p>
+                <p>More: <a href="/images/base_drawing.gif">base_drawing</a></p>
             </div>
             <div class="box">
                 <?php fp_img("base_hello.gif"); ?>
@@ -117,19 +127,36 @@
 
         
         <h1 style="margin-top: 50px;">2019</h1>
+
+        <h2>Coincell keychain flashlight</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Coincell flashlight</h2>
-                <p>Christmas gift for my friends. Also a pilot project for the moisture sensors. It's supposed to feel like its alive, in a sense. It periodically checks for movement and adjusts its "activity" value accordingly. The activity value determines how often (or if at all) it randomly flashes. It also has some more or less useful features like static flashlight and various flashing modes and a "game" which you cannot win.</p>
+                <p>Christmas gift for my friends. Also a pilot project for the moisture sensors. It periodically checks for movement and adjusts its "activity" value accordingly. The activity value determines how often (or if at all) it randomly flashes. It's supposed to feel like its alive, in a sense. Written out like this it doesn't sound like much but I think the effect is kinda neat and serves its purpose.</p>
+                <p>It also has some more or less useful features like static flashlight and various flashing modes and a "game" which you cannot win.</p>
                 <p>Source code and a little bit of documentation available on my <a href="https://github.com/georges-circuits/coincell_flashlight" target="_blank">GitHub</a></p>
+                <br>
+                <p><b>User Manual</b> for those I told to search for it here:
+                <ul>
+                    <li>Flick it and simultaneously press the button to get into the menu or keep holding the button and the light will come on.</li>
+                    <li>Menu:<ul>
+                        <li>Red: Exit menu</li>
+                        <li>Orange: Flashing mode 1 (exit by pressing the button, no timeout)</li>
+                        <li>Yellow: Flashing mode 2 (exit by pressing the button, no timeout)</li>
+                        <li>Green: The game (Have fun! If you figure out how to play it, congratulate yourself)</li>
+                        <li>Blue: Suspend the random flashing for 24 hours</li>
+                        <li>Purple: Show battery level</li>
+                    </ul></li>
+                </ul></p>
+                <p>More: <a href="/images/flashlight_leds_lit.gif">flashlight_leds_lit</a></p>
             </div>
             <div class="box">
                 <?php fp_img("coincell_flashlight.gif"); ?>
             </div>
         </div>
+
+        <h2>Portable Bluetooth speaker</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Portable Bluetooth speaker</h2>
                 <ul>
                     <li>Amplifiers: <b>2x10W</b> AB class</li>
                     <li>Battery: <b>37 Wh</b></li>
@@ -139,8 +166,9 @@
                     <li>Charging power: 15 W max</li>
                     <li>Requisite over-temperature and overload protections including error displays</li>
                 </ul>
-                <b><p>Participted in the electronics <a href="https://www.roznovskastredni.cz/aktuality/mistrovstvi-cr-v-radiotelektronice-deti-a-mladeze-2019" target="_blank">competition</a> in Rožnov pod Radhoštěm.</p></b>
-                <p><a href="/images/speaker_inside_1.gif">speaker_inside_1</a>, <a href="/images/speaker_inside_2.gif">speaker_inside_2</a>, <a href="/images/speaker_diagram_cz.gif">speaker_diagram_cz</a>, <a href="/images/speaker_layer_model.gif">speaker_layer_model</a>, <a href="/images/speaker_io_panel.gif">speaker_io_panel_cz</a></p>
+                <p>Almost everything is custom designed (except the Bluetooth module and the two power converters visible <a href="/images/speaker_inside_1.gif">here</a>, but those were modified to be controllable by the microcontroller). The enclosure is made out of <a href="/images/speaker_layer_model.gif">layers</a> of laser cut plywood, the back is 3 mm aluminum for structural and cooling purposes. It's not the most efficient speaker in the world because it uses AB amplifiers. Pretty much all portable speakers use class D, which are way more efficient but higher distortion.</p>
+                <p><b>Participated in the electronics <a href="https://www.roznovskastredni.cz/aktuality/mistrovstvi-cr-v-radiotelektronice-deti-a-mladeze-2019" target="_blank">competition</a> in Rožnov pod Radhoštěm.</b></p>
+                <p>More: <a href="/images/speaker_inside_1.gif">speaker_inside_1</a>, <a href="/images/speaker_inside_2.gif">speaker_inside_2</a>, <a href="/images/speaker_diagram_cz.gif">speaker_diagram_cz</a>, <a href="/images/speaker_layer_model.gif">speaker_layer_model</a>, <a href="/images/speaker_io_panel.gif">speaker_io_panel_cz</a></p>
             </div>
             <div class="box">
                 <?php fp_img("bt_speaker.gif"); ?>
@@ -148,23 +176,51 @@
         </div>
 
         
-        <h1 style="margin-top: 50px;">2018</h1>
+        <!-- <h1 style="margin-top: 50px;">2018</h1>
+
+        <h2>Watering system controller box</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Watering system controller box</h2>
+            <p><i>Description will be added soon</i></p>
                 <p></p>
             </div>
             <div class="box">
-                <?php fp_img("controller_box.gif"); ?>
+                <?php //fp_img("controller_box.gif"); ?>
             </div>
-        </div>
+        </div> -->
 
         
-        <h1 style="margin-top: 50px;">2017</h1>
+        <h1 style="margin-top: 50px;">2017/18</h1>
+
+        <h2>Quadcopters!</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Quadcopters!</h2>
+                <p>The one you can see on the right is my newest one:
+                <ul>
+                    <li>Quad itself:<ul>
+                        <li>Frame: <b>Martian II</b></li>
+                        <li>Motors: <b>BrotherHobby R4 2300KV</b></li>
+                        <li>Speed controllers (ESCs): <b>Racerstar LiteS 30A</b></li>
+                        <li>Flight controller (FC): <b>Betaflight F3</b></li>
+                        <li>Radio receiver: <b>FrSky X4R</b></li>
+                        <li>FPV Camera: <b>RunCam micro Sparrow 2</b></li>
+                        <li>FPV video transmitter (VTX): <b>Eachine TX526</b></li>
+                        <li>FPV 5.8 GHz antenna: Some DYS antenna wrapped in heatshrink</li>
+                        <li>Propellers: <b>Dalprop T5046</b> (stable, well rounded, durable, little slow but fun), <b>Emax AVAN-R 5065</b> (the most aggressive props I know, extremely high power draw, they give you a sense of absolute control, amazing handling, snappiness and immediate response to input, really fast and a joy to fly but will destroy your batteries and reduce flight time by 30%), <b>Gemfan 5152 triblade</b> (first prop I tried that actually felt different, relatively high speed, nice handling, little heavy, durable to a point)</li>
+                    </ul></li>
+                    <li>Batteries:<ul>
+                        <li>These things are, in my opinion, holding this hobby back the most. Quads are extremely power hungry and are constantly pushing batteries way beyond their limits, as a result, the batteries only last anywhere from 30 to 100 flights, which makes them probably the most expensive part of this hobby in the long term.</li>
+                        <li><b>Turnigy Graphene 4S 1300mAh</b> 65C (mine are pretty much dead after ~60 flights. I've had 6 in total: destroyed one in a crash, 3 are starting to show signs of dying cells, still flyable but weak in punchouts and aggresive corners and the remaining two are ok)</li>
+                        <li><b>Turnigy Multistar 4S 1400mAh</b> 65C (these have lasted quite a bit longer with some clocking in about 150 flights, but they were weaker throughout compared to the Graphenes)</li>
+                    </ul></li>
+                    <li>Ground equipment:<ul>
+                        <li>Radio controller: <b>FrSky QX7</b></li>
+                        <li>FPV goggles: <b>Aomway Commanders</b></li>
+                        <li>Charger: <b>ISDT SC-608</b></li>
+                    </ul></li>
+                </ul></p>
                 <p></p>
+                <p>More: <a href="/images/quad_scenique.gif">quad_scenique</a></p>
             </div>
             <div class="box">
                 <?php fp_img("quad_1.gif"); ?>
@@ -173,27 +229,33 @@
 
 
         <h1 style="margin-top: 50px;">2016</h1>
+
+        <h2>Arduino-based multicell Li-Po or Lead-acid battery charger</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Arduino-based multicell Li-Po or Lead-acid battery charger</h2>
+            <p><i>Description will be added soon</i></p>
                 <p>More: <a href="/images/charger_pcb_1.gif">charger_pcb_1</a>, <a href="/images/charger_pcb_2.gif">charger_pcb_2</a></p>
             </div>
             <div class="box">
                 <?php fp_img("charger.gif"); ?>
             </div>
         </div>
+
+        <h2>Obstacle avoidance robot</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Obstacle avoidance robot</h2>
+                <p><i>Description will be added soon</i></p>
                 <p>More: <a href="/images/robot_driving.gif">robot_driving</a></p>
             </div>
             <div class="box">
                 <?php fp_img("robot_top.gif"); ?>
             </div>
         </div>
+
+        <h2>Arduino-based MPPT battery solar charge controller v2.0</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Arduino-based MPPT battery charge controller v2.0</h2>
+                <p><i>Description will be added soon</i></p>
                 <p></p>
                 <p>More images: <a href="/images/mppt_inside.gif">mppt_inside</a>, <a href="/images/mppt_pcb.gif">mppt_pcb</a></p>
             </div>
@@ -201,9 +263,11 @@
                 <?php fp_img("mppt_display.gif"); ?>
             </div>
         </div>
+
+        <h2>Power bank</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Power bank</h2>
+                <p><i>Description will be added soon</i></p>
                 <p>More images: <a href="/images/power_bank_inside.gif">power_bank_inside</a>, <a href="/images/power_bank_pcb.gif">power_bank_pcb</a></p>
             </div>
             <div class="box">
@@ -213,9 +277,10 @@
 
         
         <h1 style="margin-top: 50px;">2015</h1> 
+
+        <h2>Arduino "smart watch"</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Arduino "smart watch"</h2>
                 <ul>
                     <li>Arduino pro mini</li>
                     <li>0.96" OLED display</li>
@@ -229,10 +294,12 @@
                 <?php fp_img("arduino_smartwatch.gif"); ?>
             </div>
         </div>
+
+        <h2>Arduino-based MPPT battery solar charge controller v1.0</h2>
         <div class="clearfix">
             <div class="box">
-                <h2>Arduino-based MPPT battery charge controller v1.0</h2>
-                <p></p>
+                <p><i>Description will be added soon</i></p>
+                <p>I obviously took some inspiration from Julian Ilett's <a href="https://www.youtube.com/watch?v=MSz4-cr3EJw" target="_blank">MPPT</a>.</p>
                 <p>More images: <a href="/images/mppt_v1_display.gif">mppt_v1_display</a></p>
             </div>
             <div class="box">
@@ -244,7 +311,8 @@
         <footer style="margin-top: 100px; font-size: 14px;">
             <p><i>Work in progress...</i><br>
             České verze se to taky <b>možná</b> někdy dočká.</p>
-            <p>Up since 3<sup>rd</sup> January 2020</p>
+            <p>Up since 3<sup>rd</sup> January 2020 <br>
+            Last update: 12.1.2020</p>
         </footer>
     </body>
 </html>
